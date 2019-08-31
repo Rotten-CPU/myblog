3d08e7e8d07bfd4a0758c77f186680791817a069
zhangpc <350803393@qq.com>
2019-08-31 10:39:53 +0800

    2019-08-31 10:39:53提交数据

--- push.sh
+++ push.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 echo "执行：git add ."
 git add .
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo "执行：git commit -m 当前时间"
 shijian=`date +%F' '%H:%M:%S'提交数据'`
 echo $shijian
 git commit -m "$shijian"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo "执行：git push origin dev"
+git push origin dev
